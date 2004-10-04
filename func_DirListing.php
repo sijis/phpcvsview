fba6e7e120e10228ca0a0cfe29d7028d0b08fd5d
Brian A Cheeseman <bcheesem@users.sourceforge.net>
2004-10-04 11:51:32 +0000

    Moved XHTML code generation to the theme.

--- func_DirListing.php
+++ func_DirListing.php
@@ -13,14 +13,8 @@
 
 function DisplayDirListing() {
 	global $ModPath, $CVSROOT, $PServer, $UserName, $Password, $ScriptName, 
-	       $HTMLTitle, $HTMLHeading, $HTMLTblHdBg, $HTMLTblCell1, $HTMLTblCell2;
+	       $HTMLTitle, $HTMLHeading, $FolderIcon, $FileIcon, $RowClass;
 
-	// Calculate the path from the $ScriptName variable.
-	$ScriptPath = substr($ScriptName, 0, strrpos($ScriptName, "/"));
-	if ($ScriptPath == "") {
-	    $ScriptPath = "/";
-	}
-		  
 	// Create our CVS connection object and set the required properties.
 	$CVSServer = new CVS_PServer($CVSROOT, $PServer, $UserName, $Password);
 	
@@ -40,84 +34,26 @@ function DisplayDirListing() {
 		$CVSServer->RLog($ModPath);
 		
 		// Add the quick link navigation bar.
-		$Dirs = explode("/", $ModPath);
-		echo "<div class=\"quicknav\">Navigate to: <a href=\"$ScriptName\">Root</a>&nbsp;";
-		$intCount = 1;
-		while($intCount < count($Dirs)-2){
-			echo "/&nbsp;<a href=\"$ScriptName?mp=".ImplodeToPath($Dirs, "/", $intCount)."/\">".$Dirs[$intCount]."</a>&nbsp;";
-			$intCount++;
-		} // while
-		echo "/&nbsp;".$Dirs[$intCount]."</div>\n";
+		echo GetQuickLinkBar($ModPath);
 		
 		// Start the output for the table.
-		echo "<hr />\n";
-		echo "<table>\n";
-		echo "  <tr class=\"head\">\n    <th>&nbsp;</th>\n    <th>File</th>\n    <th>Rev.</th>\n    <th>Age</th>\n    <th>Author</th>\n    <th>Last Log Entry</th>\n  </tr>\n";
-		$RowClass = "row1";
+		startDirList();
 		
 		// Do we need the "Back" operation.
 		if (strlen($ModPath) > 1) {
-			$HREF = str_replace("//", "/", "$ScriptName?mp=".substr($ModPath, 0, strrpos(substr($ModPath, 0, -1), "/"))."/"); 
-			echo "  <tr class=\"$RowClass\">\n";
-			echo "    <td align=\"center\"><a href=\"$HREF\"><img alt=\"parent\" src=\"$ScriptPath/images/parent.png\" /></a></td>\n";
-			echo "    <td><a href=\"$HREF\">Previous Level</a></td>\n";
-			echo "    <td>&nbsp;</td>\n";
-			echo "    <td>&nbsp;</td>\n";
-			echo "    <td>&nbsp;</td>\n";
-			echo "    <td>&nbsp;</td>\n";
-			echo "  </tr>\n";
-			$RowClass = "row2";
+			addParentDirectory($ScriptName, $ScriptPath, $ModPath);
 		}
 
-		// Process each folder and display a single row in a table.		
-		foreach ($CVSServer->FOLDERS as $Folder)
-		{
-			$HREF = str_replace("//", "/", "$ScriptName?mp=$ModPath/".$Folder["Name"]."/"); 
-			echo "  <tr class=\"$RowClass\">\n";
-			echo "    <td align=\"center\"><a href=\"$HREF\"><img alt=\"DIR\" src=\"$ScriptPath/images/folder.png\" /></a></td>\n";
-			echo "    <td><a href=\"$HREF\">".$Folder["Name"]."</a></td>\n";
-			echo "    <td>&nbsp;</td>\n";
-			echo "    <td>&nbsp;</td>\n";
-			echo "    <td>&nbsp;</td>\n";
-			echo "    <td>&nbsp;</td>\n";
-			echo "  </tr>\n";
-			if ($RowClass == "row1") {
-			    $RowClass = "row2";
-			}
-			else
-			{
-				$RowClass = "row1";
-			}
-		}
+		// Display the folders within the table.
+		addFolders($ModPath, $CVSServer->FOLDERS);
 
-		foreach ($CVSServer->FILES as $File)
-		{
-			$HREF = str_replace("//", "/", "$ScriptName?mp=$ModPath/".$File["Name"]); 
-			$DateTime = strtotime($File["Revisions"][$File["Head"]]["date"]);
-			$AGE = CalculateDateDiff($DateTime, strtotime(gmdate("M d Y H:i:s")));
-			echo "  <tr class=\"$RowClass\" valign=\"top\">\n";
-			echo "    <td align=\"center\"><a href=\"$HREF&amp;fh\"><img alt=\"FILE\" src=\"$ScriptPath/images/file.png\" /></a></td>\n";
-			echo "    <td><a href=\"$HREF&amp;fh\">".$File["Name"]."</a></td>\n";
-			echo "    <td align=\"center\"><a href=\"$HREF&amp;fv&amp;dt=$DateTime\">".$File["Head"]."</a></td>\n";
-			echo "    <td align=\"center\">".$AGE." ago</td>\n";
-			echo "    <td align=\"center\">".$File["Revisions"][$File["Head"]]["author"]."</td>\n";
-			echo "    <td>".str_replace("\n", "<br />", $File["Revisions"][$File["Head"]]["LogMessage"])."</td>\n";
-			echo "  </tr>\n";
-			if ($RowClass == "row1") {
-			    $RowClass = "row2";
-			}
-			else
-			{
-				$RowClass = "row1";
-			}
-		}
-		
-		$CVSServer->Disconnect();
+		// Display the files within the table.
+		addFiles($ModPath, $CVSServer->FILES);
 		
 		// Close off our HTML table.
-		echo "  </table>\n";
-		echo "<hr />";
+		endDirList();
 
+		$CVSServer->Disconnect();
 	} else { // Else of if ($Response !== true)
 		echo "Connection Failed.";
 	} // End of if ($Response !== true)
