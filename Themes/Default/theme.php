86999e4e82904403b47a807b4eea44b2220f71fe
Sijis Aviles <sijis@users.sourceforge.net>
2004-10-06 08:49:58 +0000

    - changed statements to handle enviroment data array - minor cosmetic changes

--- Themes/Default/theme.php
+++ Themes/Default/theme.php
@@ -10,7 +10,7 @@
  * @version $Id$
  * @copyright 2003-2004 Brian A Cheeseman
  **/
- 
+
 $FolderIcon = "Themes/Default/Images/folder.gif";
 $FileIcon = "Themes/Default/Images/file.gif";
 $ParentIcon = "Themes/Default/Images/parent.gif";
@@ -24,17 +24,19 @@ function GetPageHeader($Title="", $Heading="") {
 	    $PageHead .= "<head><title>$Title</title>";
 		$PageHead .= "<link href=\"Themes/Default/theme.css\" rel=\"stylesheet\" type=\"text/css\" />";
 		$PageHead .= "</head>";
-	} // End of if ($Title != "")
+	}
+
 	$PageHead .= "<body>";
 	if ($Heading != "") {
 	    $PageHead .= "<div class=\"title\">$Heading</div>";
-	} // End of if ($Header != "")
+	}
+
 	$PageHead .= "<p>Welcome to our CVS Repository viewer. This page has been dynamically";
 	$PageHead .= " created with '<a href=\"http://phpcvsview.sourceforge.net/\">phpCVS";
 	$PageHead .= "Viewer</a>' created by <a href=\"mailto:bcheesem@users.sourceforge.net";
 	$PageHead .= "\">Brian Cheeseman</a>.</p><p>Please feel free to browse our source code.</p>";
 	return $PageHead;
-} // End of function GetPageHeader($Title="", $Heading="")
+}
 
 function GetPageFooter() {
 	global $StartTime;
@@ -45,39 +47,40 @@ function GetPageFooter() {
 	$PageFoot .= "</div>";
 	$PageFoot .= "</body></html>";
 	return $PageFoot;
-} // End of function GetPageFooter()
+}
 
 function GetQuickLinkBar($ModPath = "/", $Prefix = "Navigate to: ", $LinkLast = false, $LastIsFile = false, $Revision = "")
 {
+	global $env;
+
 	// Add the quick link navigation bar.
 	$Dirs = explode("/", $ModPath);
-	$QLOut = "<div class=\"quicknav\">$Prefix<a href=\"$ScriptName\">Root</a>&nbsp;";
+	$QLOut = "<div class=\"quicknav\">$Prefix<a href=\"".$env['script_name']."\">Root</a>&nbsp;";
 	$intCount = 1;
 	$OffSet = 2;
 	if ($LastIsFile) {
 	    $OffSet = 1;
 	}
-	while($intCount < count($Dirs)-$OffSet){
+
+	while($intCount < count($Dirs)-$OffSet) {
 		if (($intCount != count($Dirs)-$OffSet)) {
-			$QLOut .= "/&nbsp;<a href=\"$ScriptName?mp=".ImplodeToPath($Dirs, "/", $intCount)."/\">".$Dirs[$intCount]."</a>&nbsp;";
-		}
-		else
-		{
+			$QLOut .= "/&nbsp;<a href=\"".$env['script_name']."?mp=".ImplodeToPath($Dirs, "/", $intCount)."/\">".$Dirs[$intCount]."</a>&nbsp;";
+		} else {
 			$QLOut .= "/&nbsp;".$Dirs[$intCount]."&nbsp;";
 		}
 		$intCount++;
-	} // while
+	}
+
 	$QLOut .= "/&nbsp;";
 	if ($LinkLast) {
-	    $QLOut .= "<a href=\"$ScriptName?mp=".ImplodeToPath($Dirs, "/", $intCount);
+	    $QLOut .= "<a href=\"".$env['script_name']."?mp=".ImplodeToPath($Dirs, "/", $intCount);
 		if ($LastIsFile) {
 		    $QLOut .= "&amp;fh#rd$Revision\">";
-		}
-		else 
-		{
+		} else {
 			$QLOut .= "/";
 		}
 	}
+
 	$QLOut .= $Dirs[$intCount];
 	if ($LinkLast) {
 	    $QLOut .= "</a>";
@@ -103,10 +106,10 @@ function endDirList()
 
 function addParentDirectory($ModPath)
 {
-	global $RowClass, $ParentIcon, $ScriptName, $ScriptPath;
-	$HREF = str_replace("//", "/", "$ScriptName?mp=".substr($ModPath, 0, strrpos(substr($ModPath, 0, -1), "/"))."/"); 
+	global $RowClass, $ParentIcon, $env;
+	$HREF = str_replace("//", "/", $env['script_name']."?mp=".substr($ModPath, 0, strrpos(substr($ModPath, 0, -1), "/"))."/");
 	echo "  <tr class=\"$RowClass\">\n";
-	echo "    <td align=\"center\"><a href=\"$HREF\"><img alt=\"parent\" src=\"$ScriptPath/$ParentIcon\" /></a></td>\n";
+	echo "    <td align=\"center\"><a href=\"$HREF\"><img alt=\"parent\" src=\"".$env['script_path']."/$ParentIcon\" /></a></td>\n";
 	echo "    <td><a href=\"$HREF\">Up one folder</a></td>\n";
 	echo "    <td>&nbsp;</td>\n";
 	echo "    <td>&nbsp;</td>\n";
@@ -118,12 +121,11 @@ function addParentDirectory($ModPath)
 
 function addFolders($ModPath, $Folders)
 {
-	global $RowClass, $FolderIcon, $ScriptName, $ScriptPath;
-	foreach ($Folders as $Folder)
-	{
-		$HREF = str_replace("//", "/", "$ScriptName?mp=$ModPath/".$Folder["Name"]."/"); 
+	global $RowClass, $FolderIcon, $env;
+	foreach ($Folders as $Folder) {
+		$HREF = str_replace("//", "/", $env['script_name']."?mp=$ModPath/".$Folder["Name"]."/");
 		echo "  <tr class=\"$RowClass\">\n";
-		echo "    <td align=\"center\"><a href=\"$HREF\"><img alt=\"DIR\" src=\"$ScriptPath/$FolderIcon\" /></a></td>\n";
+		echo "    <td align=\"center\"><a href=\"$HREF\"><img alt=\"DIR\" src=\"".$env['script_path']."/$FolderIcon\" /></a></td>\n";
 		echo "    <td><a href=\"$HREF\">".$Folder["Name"]."</a></td>\n";
 		echo "    <td>&nbsp;</td>\n";
 		echo "    <td>&nbsp;</td>\n";
@@ -132,9 +134,7 @@ function addFolders($ModPath, $Folders)
 		echo "  </tr>\n";
 		if ($RowClass == "row1") {
 		    $RowClass = "row2";
-		}
-		else
-		{
+		} else {
 			$RowClass = "row1";
 		}
 	}
@@ -142,14 +142,13 @@ function addFolders($ModPath, $Folders)
 
 function addFiles($ModPath, $Files)
 {
-	global $RowClass, $FileIcon, $ScriptName, $ScriptPath;
-	foreach ($Files as $File)
-	{
-		$HREF = str_replace("//", "/", "$ScriptName?mp=$ModPath/".$File["Name"]); 
+	global $RowClass, $FileIcon, $env;
+	foreach ($Files as $File) {
+		$HREF = str_replace("//", "/", $env['script_name']."?mp=$ModPath/".$File["Name"]);
 		$DateTime = strtotime($File["Revisions"][$File["Head"]]["date"]);
 		$AGE = CalculateDateDiff($DateTime, strtotime(gmdate("M d Y H:i:s")));
 		echo "  <tr class=\"$RowClass\" valign=\"top\">\n";
-		echo "    <td align=\"center\"><a href=\"$HREF&amp;fh\"><img alt=\"FILE\" src=\"$ScriptPath/$FileIcon\" /></a></td>\n";
+		echo "    <td align=\"center\"><a href=\"$HREF&amp;fh\"><img alt=\"FILE\" src=\"".$env['script_path']."/$FileIcon\" /></a></td>\n";
 		echo "    <td><a href=\"$HREF&amp;fh\">".$File["Name"]."</a></td>\n";
 		echo "    <td align=\"center\"><a href=\"$HREF&amp;fv&amp;dt=$DateTime\">".$File["Head"]."</a></td>\n";
 		echo "    <td align=\"center\">".$AGE." ago</td>\n";
@@ -158,9 +157,7 @@ function addFiles($ModPath, $Files)
 		echo "  </tr>\n";
 		if ($RowClass == "row1") {
 		    $RowClass = "row2";
-		}
-		else
-		{
+		} else {
 			$RowClass = "row1";
 		}
 	}
