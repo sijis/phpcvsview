6f6d9fc012ce945d4a308620eeb3df0323ea4ac4
Brian A Cheeseman <bcheesem@users.sourceforge.net>
2004-12-22 11:14:51 +0000

    - Removed unused configuration variables from config.php

--- config.php
+++ config.php
@@ -11,8 +11,8 @@
  * @copyright 2003-2004 Brian A Cheeseman
  **/
 
-// The CVSROOT path to access. For sourceforge you need the usual expansion
-// of the path based on the project name.
+// The CVSROOT path to access as it is on the server, ie for this projects
+// repository the value should be "/cvsroot/phpcvsview"
 $config['cvsroot'] = "/cvsroot/phpcvsview";
 
 // The hostname (or IP Address) of the server providing the PServer services.
@@ -23,15 +23,6 @@ $config['username'] = "anonymous";
 
 // The password associated with the username above for authentication process.
 $config['password'] = "";
-
-// The HTMLTitle and HTMLHeading are used purely for the generation of the
-// resultant web pages.
-$config['html_title'] = "phpCVSView Source Code Library";
-$config['html_header'] = "phpCVSView Source Code Library";
-
-$config['html_tbl_head_bg'] = "#CCCCCC";
-$config['html_tbl_cell_one'] = "#FFFFFF";
-$config['html_tbl_cell_two'] = "#CCCCEE";
 
 // Setup whether to use GeSHi project code for syntax highlighting or not.
 $config['UseGeSHi'] = true;
