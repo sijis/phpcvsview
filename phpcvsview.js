f8ab70149b914d9a8028dfbae9a84e46d61d8d41
Brian A Cheeseman <bcheesem@users.sourceforge.net>
2005-01-23 04:56:09 +0000

    Added javascript function for posting back the File Diff request.

--- phpcvsview.js
+++ phpcvsview.js
@@ -12,3 +12,25 @@ function postBackThemeChange(form)
 	}
 	location=newlocation;
 }
+
+function postBackDiffRequest(form)
+{
+	var ddRev1=form.DiffRev1.value;
+	var ddRev2=form.DiffRev2.value;
+	if (ddRev1 > ddRev2)
+	{
+		// Swap the values.
+		var ddTemp = ddRev1;
+		ddRev1 = ddRev2;
+		ddRev2 = ddTemp;
+	}
+	if (ddRev1 == ddRev2)
+	{
+		alert('Cannot generate a diff of a revision to itself!')
+	}
+	else
+	{
+		var dfDiffReq=form.URLDiffReq.value;
+		location=dfDiffReq+'&r1='+ddRev1+'&r2='+ddRev2+'&df';
+	}
+}
