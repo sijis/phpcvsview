9334d6dbecc94bb89b9a62b4bb762d578a07169f
Sijis Aviles <sijis@users.sourceforge.net>
2005-02-01 15:14:28 +0000

    - added function for internationalization (i18n) support - updated copyright year - minor code cleanup

--- utils.php
+++ utils.php
@@ -8,85 +8,82 @@
  *
  * @author Brian A Cheeseman <bcheesem@users.sourceforge.net>
  * @version $Id$
- * @copyright 2003-2004 Brian A Cheeseman
+ * @copyright 2003-2005 Brian A Cheeseman
  **/
 
-function microtime_diff($a, $b) {
-   list($a_dec, $a_sec) = explode(" ", $a);
-   list($b_dec, $b_sec) = explode(" ", $b);
-   return $b_sec - $a_sec + $b_dec - $a_dec;
-} // End of function microtime_diff($a, $b)
+function microtime_diff($a, $b)
+{
+	list($a_dec, $a_sec) = explode(" ", $a);
+	list($b_dec, $b_sec) = explode(" ", $b);
+	return $b_sec - $a_sec + $b_dec - $a_dec;
+}
 
 function CalculateDateDiff($DateEarlier, $DateLater)
 {
-	$DateDiff = $DateLater - $DateEarlier;
-	$Seconds = $DateDiff;
-	$Minutes = floor($Seconds/60);
-	$Hours = floor($Minutes/60);
-	$Days = floor($Hours/24);
-	$Weeks = floor($Days/7);
-	$Years = floor($Days/365);
+	global $lang;
 
-	if ($Seconds > 0) {
-	    $Result = "$Seconds Second";
-		if ($DateDiff > 1) {
-		    $Result .= "s";
+	$date['date_diff'] = $DateLater - $DateEarlier;
+	$date['seconds'] = $date['date_diff'];
+	$date['minutes'] = floor($date['seconds']/60);
+	$date['hours'] = floor($date['minutes']/60);
+	$date['days'] = floor($date['hours']/24);
+	$date['weeks'] = floor($date['days']/7);
+	$date['years'] = floor($date['days']/365);
+
+	// displays seconds
+	if ($date['seconds'] > 0) {
+	    $Result = $date['seconds'] .' ';
+		$Result .= ($date['date_diff'] > 1)? $lang['seconds'] : $lang['second'];
+	}
+
+	// displays minutes
+	if ($date['minutes'] > 0) {
+	    $Result = $date['minutes'].' ';
+		$Result .= ($date['minutes'] > 1)? $lang['minutes'] : $lang['minute'];
+	}
+
+	// displays hours, then minutes
+	if ($date['hours'] > 0) {
+	    $Result = $date['hours'].' ';
+		$Result .= ($date['hours'] > 1)? $lang['hours'] : $lang['hour'];
+		$date['minutes'] = $date['minutes'] % 60;
+		if ($date['minutes'] > 0) {
+		    $Result .= ', '.$date['minutes'].' ';
+			$Result .= ($date['minutes'] > 1)? $lang['minutes'] : $lang['minute'];
 		}
 	}
-	if ($Minutes > 0) {
-	    $Result = "$Minutes Minute";
-		if ($Minutes > 1) {
-		    $Result .= "s";
+
+	// displays days, then hours
+	if ($date['days'] > 0) {
+	    $Result = $date['days'] . ' ';
+		$Result .= ($date['days'] > 1)? $lang['days'] : $lang['day'];
+		$date['hours'] = $date['hours'] % 24;
+		if ($date['hours'] > 0) {
+		    $Result .= ', '.$date['hours'].' ';
+			$Result .= ($date['hours'] > 1)? $lang['hours'] : $lang['hour'];
 		}
 	}
-	if ($Hours > 0) {
-	    $Result = "$Hours Hour";
-		if ($Hours > 1) {
-		    $Result .= "s";
-		}
-		$Minutes = $Minutes % 60;
-		if ($Minutes > 0) {
-		    $Result .= ", $Minutes Minute";
-			if ($Minutes > 1) {
-			    $Result .= "s";
-			}
+
+	// displays weeks, then days
+	if ($date['weeks'] > 0) {
+	    $Result = $date['weeks'] . ' ';
+		$Result .= ($date['weeks'] > 1)? $lang['weeks'] : $lang['week'];
+		$date['days'] = $date['days'] % 7;
+		if ($date['days'] > 0) {
+		    $Result .= ', '.$date['days'].' ';
+			$Result .= ($date['days'] > 1)? $lang['days'] : $lang['day'];
 		}
 	}
-	if ($Days > 0) {
-	    $Result = $Days . " Day";
-		if ($Days > 1) {
-		    $Result .= "s";
-		}
-		$Hours = $Hours % 24;
-		if ($Hours > 0) {
-		    $Result .= ", $Hours Hour";
-			if ($Hours > 1) {
-			    $Result .= "s";
-			}
-		}
-	}
-	if ($Weeks > 0) {
-	    $Result = $Weeks . " Week";
-		if ($Days > 1) {
-		    $Result .= "s";
-		}
-		$Days = $Days % 7;
-		if ($Days > 0) {
-		    $Result .= ", $Days Day";
-			if ($Days > 1) {
-			    $Result .= "s";
-			}
-		}
-	}
-	if ($Years > 0) {
-		$Result = $Years . " Year";
-		if ($Years > 1) {
-		    $Result .= "s";
-		}
-		$Weeks = $Weeks % 52;
-		if ($Weeks > 0) {
-		    $Result .= ", $Weeks Week";
-			if ($Weeks > 1) {
+
+	// displays years, then weeks
+	if ($date['years'] > 0) {
+		$Result = $date['years'] . ' ';
+		$Result .= ($date['years'] > 1)? $lang['years'] : $lang['year'];
+		$date['weeks'] = $date['weeks'] % 52;
+		if ($date['weeks'] > 0) {
+		    $Result .= ', '.$date['weeks'].' ';
+			$Result .= ($date['weeks'] > 1)? $lang['weeks'] : $lang['week'];
+			if ($date['weeks'] > 1) {
 			    $Result .= "s";
 			}
 		}
@@ -100,7 +97,7 @@ function ImplodeToPath($Dirs, $Seperator, $Number)
 	for ($Counter = 0; $Counter <= $Number; $Counter++)
 	{
 		if ($Dirs[$Counter] != "") {
-		    $RetVal .= $Seperator . $Dirs[$Counter];
+			$RetVal .= $Seperator . $Dirs[$Counter];
 		}
 	}
 	return $RetVal;
@@ -108,22 +105,45 @@ function ImplodeToPath($Dirs, $Seperator, $Number)
 
 function GetThemeList()
 {
-	$DirHandle = opendir('Themes');
-	$ThemeList = Array();
-	$ThemeCount = 0;
-	while ($Dir = readdir($DirHandle)) 
-	{
-		if ((!ereg("[.]",$Dir)))
-		{
-			if($Dir != "CVS") 
-			{
-				$ThemeList[$ThemeCount++] = $Dir;
+	global $env;
+	$theme = array();
+
+	// open theme directory
+	if ($handle = opendir($env['theme_path'])) {
+
+		// list all directories
+		while (false !== ($file = readdir($handle))) {
+			// do not list . and ..
+			if ($file != "." && $file != "..") {
+				// add directory to an array
+				array_push($theme, $file);
 			}
 		}
+		closedir($handle);
 	}
-	closedir($DirHandle);
-	sort($ThemeList, SORT_STRING);
-	return $ThemeList;
+	sort($theme, SORT_STRING);
+	return $theme;
+}
+
+function GetLanguagesList()
+{
+	global $env;
+	$lang = array();
+
+	// open language directory
+	if ($handle = opendir($env['language_path'])) {
+
+		// list all directory files
+		while (false !== ($file = readdir($handle))) {
+			// do not list . and ..
+			if ($file != "." && $file != "..") {
+				// strip filename and add to an array
+				array_push($lang, rtrim($file, '.php'));
+			}
+		}
+		closedir($handle);
+	}
+	return $lang;
 }
 
 ?>
