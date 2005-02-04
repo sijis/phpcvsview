7bfc06c2b958294e42e4cd823a3fb50544cc1980
Brian A Cheeseman <bcheesem@users.sourceforge.net>
2005-02-04 23:14:24 +0000

    Added language file for Portuguese Brazilian as translated by Giuliano Camarini de Oliveira

---
+++ languages/pt_br.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * This source code is distributed under the terms as layed out in the
+ * GNU General Public License.
+ *
+ * Purpose: Portuguese Brazilian translation file.
+ *
+ * @author Sijis Aviles <sijis@users.sourceforge.net>
+ * @version $Id$
+ * @copyright 2003-2005 Brian A Cheeseman
+ * 
+ * Translation completed Giuliano Camarini de Oliveira <gcoliveira@rocketmail.com> 
+ * Translation copyright 2005 Karlos Giuliano Camarini de Oliveira
+ **/
+
+$lang['encoding']		= 'iso-8859-1';
+
+// listing options
+$lang['file']			= 'Arquivo';
+$lang['rev']			= 'Rev.';
+$lang['age']			= 'Idade';
+$lang['author']			= 'Autor';
+$lang['last_log']		= 'Última Entrada Perdida';
+
+// customization options
+$lang['language']		= 'Português';
+$lang['theme']			= 'Trocar Tema: ';
+
+// quick link bar listing
+$lang['change_theme']	= 'Trocar Tema: ';
+$lang['change_lang']	= 'Língua: ';
+$lang['change_cvsroot'] = 'Repositório CVS: ';
+$lang['up_folder']		= 'Subir um diretório';
+$lang['rev_history']	= 'Histórico de revisões para: ';
+$lang['rev_diff']		= 'Diferença de revisões para: ';
+$lang['code_view']		= 'Visualização do código para: ';
+$lang['navigate_to']	= 'Navegar para: ';
+$lang['file_ext']		= 'Extensão do arquivo: ';
+$lang['annotate_history']	= 'Histórico de notas para: ';
+$lang['mime_type']		= 'Mime Type é: ';
+
+// time and date related
+$lang['year']			= 'Ano';
+$lang['years']			= 'Anos';
+$lang['week']			= 'Semana';
+$lang['weeks']			= 'Semanas';
+$lang['day']			= 'Dia';
+$lang['days']			= 'Dias';
+$lang['hour']			= 'Hora';
+$lang['hours']			= 'Horas';
+$lang['minute']			= 'Minuto';
+$lang['minutes']		= 'Minutos';
+$lang['second']			= 'Segundo';
+$lang['seconds']		= 'Segundos';
+$lang['ago']			= 'atrás';
+
+// navigation path
+$lang['root']			= 'Raiz';
+
+// top message
+$lang['message']		= '<p>Bem-vindo ao sistema de visualização de repositório CVS para o projeto phpCVSView hospedado no SourceForge.net</p><p>O objetivo deste projeto é simplesmente construir uma aplicação/classe PHP para prover acesso a um repositório de controle de fontes baseado em CVS usando vários mecanismos de conectividade disponíveis para CVS em geral. Existem também algumas extensões para este objetivo que estão planejadas para versões futuras, como um cliente CVS totalmente baseado em web utilizando o núcleo deste projeto.</p><p>Portanto, por favor sinta-se livre para olhar nosso código, sugerir novas características, testar o código no seu próprio ambiente, enviar bugs, e acima de tudo, apoiar o comprometimento dos desenvolvedores em código aberto utilizando os maravilhosos produtos disponíveis.</p><p>Atenciosamente,<br />Brian Cheeseman. <br />Líder do Projeto phpCVSView.</p>';
+
+// bottom messages
+$lang['generated']		= 'Esta página foi gerada por <a href="http://phpcvsview.sourceforge.net/">phpCVSView</a> em ';
+$lang['created_by']		= 'phpCVSView criado por <a href="mailto:bcheesem@users.sourceforge.net">Brian Cheeseman</a> e <a href="mailto:sijis@users.sourceforge.net">Sijis Aviles</a>.';
+
+// file options
+$lang['view']			= 'ver';
+$lang['download']		= 'download';
+$lang['diff']			= 'diferenças em relação ao anterior';
+$lang['annotate']		= 'notas';
+
+// file details
+$lang['revision']		= 'Revisão';
+$lang['last_checkin']	= 'Último Checkin:';
+$lang['notice']			= 'Observação:';
+$lang['branch']			= 'Seção:';
+$lang['date']			= 'Data:';
+$lang['time']			= 'Hora:';
+$lang['state']			= 'Estado:';
+$lang['changes']		= 'Mudanças desde ';
+$lang['log_message']	= 'Mensagem do Log:';
+
+// error messages
+$lang['err_connect']	= '<h2>ERRO: Falha ao conectar ao PServer.</h2>'."\n";
+$lang['err_get_rev']	= '<h3>ERRO: Solicitando Revisão para o arquivo:</h3>';
+
+?>
