d102ef9996207731056575ec2f02823c40f9d557
Brian A Cheeseman <bcheesem@users.sourceforge.net>
2005-02-04 23:12:58 +0000

    Added language file for Spanish as translated by Karlos Olmedo

---
+++ languages/es.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * This source code is distributed under the terms as layed out in the
+ * GNU General Public License.
+ *
+ * Purpose: Spanish translation file.
+ *
+ * @author Sijis Aviles <sijis@users.sourceforge.net>
+ * @version $Id$
+ * @copyright 2003-2005 Brian A Cheeseman
+ * 
+ * Translation completed Karlos Olmedo <Hazel@dubmail.net> 
+ * Translation copyright 2005 Karlos Olmedo
+ **/
+
+$lang['encoding']		= 'iso-8859-1';
+
+// listing options
+$lang['file']			= 'Archivo';
+$lang['rev']			= 'Rev.';
+$lang['age']			= 'Edad';
+$lang['author']			= 'Autor';
+$lang['last_log']		= 'Última entrada perdida';
+
+// customization options
+$lang['language']		= 'Español';
+$lang['theme']			= 'Cambiar estilo: ';
+
+// quick link bar listing
+$lang['change_theme']	= 'Cambiar estilo: ';
+$lang['change_lang']	= 'Idioma: ';
+$lang['change_cvsroot'] = 'Repositorio CVS: ';
+$lang['up_folder']		= 'Subir una carpeta';
+$lang['rev_history']	= 'Historial de la Revisión: ';
+$lang['rev_diff']		= 'Diferencias de Revisión: ';
+$lang['code_view']		= 'Vista de Código: ';
+$lang['navigate_to']	= 'Navegar a: ';
+$lang['file_ext']		= 'Extensión del archivo: ';
+$lang['annotate_history']	= 'Anotación del historial: ';
+$lang['mime_type']		= 'Tipo MIME: ';
+
+// time and date related
+$lang['year']			= 'Año';
+$lang['years']			= 'Años';
+$lang['week']			= 'Semana';
+$lang['weeks']			= 'Semanas';
+$lang['day']			= 'Día';
+$lang['days']			= 'Días';
+$lang['hour']			= 'Hora';
+$lang['hours']			= 'Horas';
+$lang['minute']			= 'Minuto';
+$lang['minutes']		= 'Minutos';
+$lang['second']			= 'Segundo';
+$lang['seconds']		= 'Segundos';
+$lang['ago']			= 'hace';
+
+// navigation path
+$lang['root']			= 'Root';
+
+// top message
+$lang['message']		= '<p>Bienvenido/a al sistema de visualización CVS Repository para el proyecto phpCVSView hospedado en SourceForge.net</p><p>El objetivo de este proyecto es simplemente construir una aplicación/clase para dar acceso a un repositorio de control de codigo CVS sobre los variados mecanismos de conexión disponibles para CVS en general. Hay además objetivos añadidos planeados para futuros lanzamientos, como un completo cliente via web utilizando el nucleo de este proyecto.</p><p>Asi que por favor sientase libre de mirar nuestro código, sugerir nuevas características, testear el código en su propio entorno, comunicar bugs, y de darle soporte al compromiso de los desarrolladores del software libre usando los muchos y maravillosos productos disponibles.</p><p>Kindest Regards,<br />Brian Cheeseman. <br />Jefe del proyecto phpCVSView.</p>';
+
+// bottom messages
+$lang['generated']		= 'Esta página ha sido generada por <a href="http://phpcvsview.sourceforge.net/">phpCVSView</a> en ';
+$lang['created_by']		= 'phpCVSView creado por <a href="mailto:bcheesem@users.sourceforge.net">Brian Cheeseman</a> y <a href="mailto:sijis@users.sourceforge.net">Sijis Aviles</a>.';
+
+// file options
+$lang['view']			= 'ver';
+$lang['download']		= 'descargar';
+$lang['diff']			= 'al anterior';
+$lang['annotate']		= 'anotar';
+
+// file details
+$lang['revision']		= 'Revisión';
+$lang['last_checkin']	= 'Ultima comprobación:';
+$lang['notice']			= 'Aviso:';
+$lang['branch']			= 'Rama:';
+$lang['date']			= 'Fecha:';
+$lang['time']			= 'Hora:';
+$lang['state']			= 'Estado:';
+$lang['changes']		= 'Cambios desde ';
+$lang['log_message']	= 'Mensaje de Log:';
+
+// error messages
+$lang['err_connect']	= '<h2>ERROR: No se puede conectar con el PServer.</h2>'."\n";
+$lang['err_get_rev']	= '<h3>ERROR: Obteniendo revisión para el archivo:</h3>';
+
+?>
