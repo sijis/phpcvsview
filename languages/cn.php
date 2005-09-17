e3aec219d449cf9dbaf7eda00ad2364965ca1827
Sijis Aviles <sijis@users.sourceforge.net>
2005-09-17 07:05:34 +0000

    - added image descriptive text

--- languages/cn.php
+++ languages/cn.php
@@ -17,12 +17,12 @@
  *           翻”了。Sail Lee <sail_lee@21cn.com>
  **/
 
-$lang['encoding']                = 'GB2312';
+$lang['encoding']		= 'GB2312';
 
 // listing options
-$lang['file']				= '文件';
-$lang['rev']				= '版次';
-$lang['age']				= '时限';
+$lang['file']			= '文件';
+$lang['rev']			= '版次';
+$lang['age']			= '时限';
 $lang['author']			= '作者';
 $lang['last_log']		= '最近更新日志';
 
@@ -36,42 +36,42 @@ $lang['change_lang']	= '语言: ';
 $lang['change_cvsroot']	= 'CVS源码库: ';
 $lang['up_folder']		= '上一目录';
 $lang['rev_history']	= '以下文件的版次历史: ';
-$lang['rev_diff']			= '以下文件的版次对比: ';
+$lang['rev_diff']		= '以下文件的版次对比: ';
 $lang['code_view']		= '以下文件的代码浏览: ';
 $lang['navigate_to']	= '目录导航: ';
-$lang['file_ext']			= '文件扩展名: ';
+$lang['file_ext']		= '文件扩展名: ';
 $lang['annotate_history']	= '以下文件的注释历史: ';
 $lang['mime_type']		= '媒体类型: ';
 
 // time and date related
-$lang['year']				= '年';
+$lang['year']			= '年';
 $lang['years']			= '年';
-$lang['week']				= '周';
+$lang['week']			= '周';
 $lang['weeks']			= '周';
-$lang['day']				= '日';
-$lang['days']				= '日';
-$lang['hour']				= '小时';
+$lang['day']			= '日';
+$lang['days']			= '日';
+$lang['hour']			= '小时';
 $lang['hours']			= '小时';
 $lang['minute']			= '分钟';
 $lang['minutes']		= '分钟';
 $lang['second']			= '秒';
 $lang['seconds']		= '秒';
-$lang['ago']				= '前';
+$lang['ago']			= '前';
 
 // navigation path
-$lang['root']				= '根目录';
+$lang['root']			= '根目录';
 
 // top message
-$lang['message']	= '<p>欢迎使用建立于SourceForge.net上phpCVSView项目的CVS源码库浏览系统。</p><p>本项目的目的就是要简单地建立一个PHP应用程序/类，在不同的CVS连接机制上，提供基于CVS源码库的存取。这个项目的以后版本还会有一些扩展成果，比如一个全面的基于WEB的CVS客户端软件，它就采用了这个项目的核心技术.</p><p>所以，欢迎大家关注我们的代码，提出你的需求，在特定的环境里测试我们的代码，提交你发现的问题，能让大家使用更多的好产品是开源开发者们的最大责任。</p><p>此致诚挚的祝愿,<br />Brian Cheeseman. <br />phpCVSView项目领导人</p>';
+$lang['message']		= '<p>欢迎使用建立于SourceForge.net上phpCVSView项目的CVS源码库浏览系统。</p><p>本项目的目的就是要简单地建立一个PHP应用程序/类，在不同的CVS连接机制上，提供基于CVS源码库的存取。这个项目的以后版本还会有一些扩展成果，比如一个全面的基于WEB的CVS客户端软件，它就采用了这个项目的核心技术.</p><p>所以，欢迎大家关注我们的代码，提出你的需求，在特定的环境里测试我们的代码，提交你发现的问题，能让大家使用更多的好产品是开源开发者们的最大责任。</p><p>此致诚挚的祝愿,<br />Brian Cheeseman. <br />phpCVSView项目领导人</p>';
 
 // bottom messages
-$lang['generated']	= '本页面由 <a href="http://phpcvsview.sourceforge.net/">phpCVSView</a> 生成，共 ';
-$lang['created_by']	= 'phpCVSView 项目由 <a href="mailto:bcheesem@users.sourceforge.net">Brian Cheeseman</a> 和 <a href="mailto:sijis@users.sourceforge.net">Sijis Aviles</a> 建立.';
+$lang['generated']		= '本页面由 <a href="http://phpcvsview.sourceforge.net/">phpCVSView</a> 生成，共 ';
+$lang['created_by']		= 'phpCVSView 项目由 <a href="mailto:bcheesem@users.sourceforge.net">Brian Cheeseman</a> 和 <a href="mailto:sijis@users.sourceforge.net">Sijis Aviles</a> 建立.';
 
 // file options
-$lang['view']				= '浏览';
+$lang['view']			= '浏览';
 $lang['download']		= '下载';
-$lang['diff']				= '和上一版对比';
+$lang['diff']			= '和上一版对比';
 $lang['annotate']		= '注释';
 
 // file details
@@ -79,14 +79,23 @@ $lang['revision']		= '版次';
 $lang['last_checkin']	= '上一检入日期:';
 $lang['notice']			= '通告:';
 $lang['branch']			= '分支:';
-$lang['date']				= '日期:';
-$lang['time']				= '时间:';
+$lang['date']			= '日期:';
+$lang['time']			= '时间:';
 $lang['state']			= '状态:';
 $lang['changes']		= '自上一版本的修改 ';
 $lang['log_message']	= '日志信息:';
 
 // error messages
-$lang['err_connect']	= '<h2>错误: 不能连接到PServer服务器。</h2>'."\n";
+$lang['err_connect']	= '<h2>错误: 不能连接到PServer服务器。</h2>';
 $lang['err_get_rev']	= '<h3>错误: 提取以下版次的文件出错:</h3>';
+
+// Image description texts.
+$lang['icon_parent']	= 'Up';
+$lang['icon_dl']		= 'D/L';
+$lang['icon_dir']		= 'Dir';
+$lang['icon_mod']		= 'Mod';
+$lang['icon_file']		= '文件';
+$lang['icon_xhtml']		= 'Valid XHTML 1.1!';
+$lang['icon_css']		= 'Valid CSS!';
 
 ?>
